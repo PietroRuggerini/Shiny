85198766f97b796c55588d34ec46cff5fc303744
PietroRuggerini <p.ruggerini@gmail.com>
2014-06-18 16:34:30 +0200

    Update UI.R

--- UI.R
+++ UI.R
@@ -3,7 +3,19 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
+  
   titlePanel("European Capitals Quiz: try to guess each capital and then click Submit!"),
+  
+   # Instructions
+  
+fluidRow("INSTRUCTIONS: For the assigment I've created a pretty simple survey, 
+where you have to associate each european capital with the correct country.
+Click on Submit! when finished and see your final score, from 0 to 9. 
+You can change your answers and retry as many times as you wish.
+The application uses 9 radio buttons (single choiche answers), arranged in 3 columns, using the fluidPage
+layout. On the Server side, a conditional operation is performed in a reactive way and 
+1 point is assigned for each correct answer. Each time the button 
+Submit is pressed, the calculation is performed again."),
   
   # Page layout with radio buttons
   
